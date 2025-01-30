e967b758bea4564d82f91b1c892712590ce55248
Mrcattington <me@kristianmarlow.com>
2025-01-30 06:15:01 -0600

    Fix layout warnings

--- app/src/main/java/com/mrcattington/ipcamscreensaver/MainActivity.kt
+++ app/src/main/java/com/mrcattington/ipcamscreensaver/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mrcattington.ipcamscreensaver.ui.theme.IPCamScreensaverTheme
